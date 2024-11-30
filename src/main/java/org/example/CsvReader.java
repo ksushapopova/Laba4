3eac5a74469c6694ddfdafd204044da071ab9c3e
ksushapopova <ks.popova.2005@mail.ru>
2024-11-30 15:06:16 +0300

    List output

--- src/main/java/org/example/CsvReader.java
+++ src/main/java/org/example/CsvReader.java
@@ -2,10 +2,12 @@ package org.example;
 
 import com.opencsv.CSVReader;
 import com.opencsv.CSVReaderBuilder;
+import com.opencsv.CSVParserBuilder;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Arrays;
 import java.io.FileNotFoundException;
 
 public class CsvReader {
@@ -22,25 +24,33 @@ public class CsvReader {
             }
 
             CSVReader csvReader = new CSVReaderBuilder(reader)
-                    .withCSVParser(new com.opencsv.CSVParserBuilder().withSeparator(separator).build())
+                    .withCSVParser(new CSVParserBuilder().withSeparator(separator).build())
                     .build();
 
             String[] nextLine;
+
+            csvReader.readNext();
+
             while ((nextLine = csvReader.readNext()) != null) {
-                int personId = Integer.parseInt(nextLine[0]);
-                String name = nextLine[1];
-                String gender = nextLine[2];
-                String departmentName = nextLine[3];
-                double salary = Double.parseDouble(nextLine[4]);
-                String birthDate = nextLine[5];
+                try {
+                    int personId = Integer.parseInt(nextLine[0]);
+                    String name = nextLine[1];
+                    String gender = nextLine[2];
+                    String birthDate = nextLine[3];
+                    String categoryCode = nextLine[4];
+                    double salary = Double.parseDouble(nextLine[5]);
 
-                Department department = new Department(departmentId++, departmentName);
-
-                Person person = new Person(personId, name, gender, department, salary, birthDate);
-                persons.add(person);
+                    Department department = new Department(departmentId++, categoryCode);
+                    Person person = new Person(personId, name, gender, department, salary, birthDate);
+                    persons.add(person);
+                } catch (NumberFormatException e) {
+                    System.err.println("Ошибка при парсинге данных: " + e.getMessage());
+                    System.err.println("Проблемная строка: " + Arrays.toString(nextLine));
+                }
             }
         }
 
         return persons;
     }
+
 }
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import java.util.List;
+
+
+public class Main {
+    public static void main(String[] args) {
+        CsvReader csvReader = new CsvReader();
+        try {
+            List<Person> persons = csvReader.readPersonsFromCsv("foreign_names.csv", ';');
+            for (Person person : persons) {
+                System.out.println("ID: " + person.getId() + ", Name: " + person.getName() +
+                        ", Gender: " + person.getGender() +
+                        ", Department: " + person.getDepartment().getName() +
+                        ", Salary: " + person.getSalary() +
+                        ", Birth Date: " + person.getBirthDate());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
